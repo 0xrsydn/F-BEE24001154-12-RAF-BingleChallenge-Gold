84c83c8a229ce9af7aa1ebdb59ca8aee14d297f1
0xrsydn <rafayrussani@gmail.com>
2024-07-03 01:54:41 +0700

    modify

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(bodyParser.json());
 
+// Import routes
 const userRoutes = require('./routes/user');
 const itemRoutes = require('./routes/item');
 const orderRoutes = require('./routes/order');
 
+// Use routes
 app.use('/users', userRoutes);
 app.use('/items', itemRoutes);
 app.use('/orders', orderRoutes);
